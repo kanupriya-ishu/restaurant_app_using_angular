06f2eb7bba84c318c3844876a332dee343553f2d
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-13 19:59:44 +0530

    Leader Service

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent]
 })
