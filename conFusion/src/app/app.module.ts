348a5e0211897ec59c6b476d597290344f5abe71
kanupriya-ishu <kanupriya2512@gmail.com>
2021-06-13 11:23:24 +0530

    Header and Footer

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -11,12 +11,16 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
